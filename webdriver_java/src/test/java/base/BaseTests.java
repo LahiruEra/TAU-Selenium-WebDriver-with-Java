637198b304b452557a3b90c5bb4cbc97e49f4810
Lahiru <Lahiru@DESKTOP-MC99MSP>
2024-06-06 21:39:46 +0530

    chapter 4.2 and 4.3

--- webdriver_java/src/test/java/base/BaseTests.java
+++ webdriver_java/src/test/java/base/BaseTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
 import pages.HomePage;
 
 public class BaseTests {
