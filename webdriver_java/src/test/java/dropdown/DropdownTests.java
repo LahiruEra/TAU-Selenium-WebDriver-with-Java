ddf810a0a968d276f227a2729c68c01f1e0c5e8a
LahiruEra <erandasiriwardhana@gmail.com>
2024-06-07 11:18:38 +0530

    Update DropdownTests.java

--- webdriver_java/src/test/java/dropdown/DropdownTests.java
+++ webdriver_java/src/test/java/dropdown/DropdownTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
+
 public class DropdownTests extends BaseTests {
 
     @Test
