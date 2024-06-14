22544019553159755a75a49220beeb505b9008ef
LahiruEra <erandasiriwardhana@gmail.com>
2024-06-14 07:08:01 +0530

    Update InfiniteScrollPage.java

--- webdriver_java/src/main/java/pages/InfiniteScrollPage.java
+++ webdriver_java/src/main/java/pages/InfiniteScrollPage.java
@@ -14,8 +14,7 @@ public class InfiniteScrollPage {
     }
 
     /**
-     * Scrolls until paragraph with index specified is in view
-     * @param index 1-based
+     * Scrolls until paragraph with index specified is in view @param index 1-based
      */
     public void scrollToParagraph(int index){
         String script = "window.scrollTo(0, document.body.scrollHeight)";
